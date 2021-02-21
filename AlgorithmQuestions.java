8d140efc4d793bf84a67473073aac6f5e7b6c207
ertugdlk <ertgdlk@gmail.com>
2021-02-21 13:00:17 +0300

    Sum reverseLists added

--- AlgorithmQuestions.java
+++ AlgorithmQuestions.java
@@ -52,17 +52,47 @@ public class AlgorithmQuestions {
         return list;
     }
 
+    // Not optimal solution
+    public static LinkedList sumReverseLists(LinkedList list1, LinkedList list2) {
+        list1 = LinkedList.reverse(list1); // O(N)
+        list2 = LinkedList.reverse(list2); // O(N)
+        StringBuilder s1 = new StringBuilder();
+        StringBuilder s2 = new StringBuilder();
+        Node currNode1 = list1.head;
+        Node currNode2 = list2.head;
+
+        // O(N)
+        while (currNode1 != null && currNode2 != null) {
+            s1.append(currNode1.value);
+            s2.append(currNode2.value);
+            currNode1 = currNode1.next;
+            currNode2 = currNode2.next;
+        }
+        Integer sum = Integer.parseInt(s1.toString()) + Integer.parseInt(s2.toString());
+        String result = sum.toString();
+        char[] chararray = result.toCharArray();
+
+        LinkedList list3 = new LinkedList();
+        for (int i = result.length() - 1; i >= 0; i--) {
+            int value = Character.getNumericValue(chararray[i]);
+            list3 = list3.insert(list3, value);
+        }
+
+        return list3;
+    }
+
     public static void main(String[] args) {
         LinkedList newlist = new LinkedList();
-        newlist = LinkedList.insert(newlist, 3);
-        newlist = LinkedList.insert(newlist, 7);
-        newlist = LinkedList.insert(newlist, 9);
-        newlist = LinkedList.insert(newlist, 7);
-        newlist = LinkedList.insert(newlist, 2);
-        LinkedList.printList(newlist);
-        newlist = sendToEnd(newlist, 2);
-        System.out.println("--------");
-        LinkedList.printList(newlist);
+        newlist = LinkedList.insert(newlist, 1);
+        newlist = LinkedList.insert(newlist, 1);
+        newlist = LinkedList.insert(newlist, 1);
+        LinkedList newlist2 = new LinkedList();
+        newlist2 = LinkedList.insert(newlist2, 1);
+        newlist2 = LinkedList.insert(newlist2, 1);
+        newlist2 = LinkedList.insert(newlist2, 1);
+
+        LinkedList list = sumLists(newlist, newlist2);
+        LinkedList.printList(list);
     }
 
 }
